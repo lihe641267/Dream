b4850669b50eeb79befcdb560c343167da0c92cb
金色VS时空 <1040152329@qq.com>
2018-03-26 21:59:07 +0800

    https://github.com/lihe641267/MyCome.git

---
+++ app/src/main/java/com/lt/vs/dream/MainActivity.java
@@ -0,0 +1,24 @@
+package com.lt.vs.dream;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Gallery;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Gallery gallery;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
